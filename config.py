3c94ff260031bcb3872599ef5b79e4cd4d80ee61
danrus32 <56735314+danrus32@users.noreply.github.com>
2020-10-18 20:42:15 +0300

    Update config.py

--- config.py
+++ config.py
@@ -8,8 +8,8 @@ import pyautogui
 import sqlite3
 
 
-token = "d27d9d1ca274a8f89a1bf7ffad503c440e96eba2ef6c2bd483ced57c7d0addfcf10fddb0f6eff541572e6"
-grupid = "199561725"
+token = ""
+grupid = ""
 v = 5.95
 def RandomId():
     a = random.randint(1,100000)
